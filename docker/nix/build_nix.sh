33b89482e5d5a171cc2ef4711afce79e015e85a2
Sam Rose <samuel@supabase.io>
2024-10-27 12:44:04 -0400

    fix: correct version

--- docker/nix/build_nix.sh
+++ docker/nix/build_nix.sh
@@ -14,4 +14,4 @@ nix build .#psql_16/bin -o psql_16
 nix build .#psql_orioledb-17/bin -o psql_orioledb_17
 nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_15
 nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_16
-nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_orioledb_16
+nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_orioledb_17
