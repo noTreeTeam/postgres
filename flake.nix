a5e8462da7b5f12f2c12db52c2b001acfc769c15
Oliver Rice <github@oliverrice.com>
2024-10-28 13:26:50 -0500

    remove partman

--- flake.nix
+++ flake.nix
@@ -151,7 +151,9 @@
           ./nix/ext/pg_hashids.nix
           ./nix/ext/pgsodium.nix
           ./nix/ext/pg_graphql.nix
-          ./nix/ext/pg_partman.nix
+          # Requires superuser but also uses dynamic sql so need to be owned by posgtres.
+          # investigating options
+          #./nix/ext/pg_partman.nix
           ./nix/ext/pg_stat_monitor.nix
           ./nix/ext/pg_jsonschema.nix
           ./nix/ext/pgvector.nix
