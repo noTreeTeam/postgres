1f955df8eedda00e3d963f68163d513b43c35d49
Sam Rose <samuel@supabase.io>
2024-11-04 09:52:18 -0500

    test: check if oriole is available and if so then enable

--- migrations/tests/test.sql
+++ migrations/tests/test.sql
@@ -5,6 +5,16 @@ BEGIN;
 
 CREATE EXTENSION IF NOT EXISTS pgtap;
 
+-- Check and create OrioleDB if available
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM pg_available_extensions WHERE name = 'orioledb') THEN
+        IF NOT EXISTS (SELECT 1 FROM pg_extension WHERE extname = 'orioledb') THEN
+            CREATE EXTENSION orioledb;
+        END IF;
+    END IF;
+END $$;
+
 SELECT no_plan();
 
 \ir fixtures.sql
