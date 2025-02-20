ef23d0dd1c2d9a93d0a6044214ea36d97880e994
Sam Rose <samuel@supabase.io>
2025-02-20 12:22:19 -0500

    feature: wal-g v 3 and v 2

--- nix/wal-g.nix
+++ nix/wal-g.nix
@@ -7,52 +7,70 @@
   installShellFiles,
 }:
 
-buildGoModule rec {
-  pname = "wal-g";
-  version = "3.0.5";
+let
+  walGCommon = { version, vendorHash, sha256 }:
+    buildGoModule rec {
+      pname = "wal-g";
+      inherit version;
 
-  src = fetchFromGitHub {
-    owner = "wal-g";
-    repo = "wal-g";
-    rev = "v${version}";
-    sha256 = "sha256-wVr0L2ZXMuEo6tc2ajNzPinVQ8ZVzNOSoaHZ4oFsA+U=";
+      src = fetchFromGitHub {
+        owner = "wal-g";
+        repo = "wal-g";
+        rev = "v${version}";
+        inherit sha256;
+      };
+
+      inherit vendorHash;
+
+      nativeBuildInputs = [ installShellFiles ];
+
+      buildInputs = [
+        brotli
+        libsodium
+      ];
+
+      subPackages = [ "main/pg" ];
+
+      tags = [
+        "brotli"
+        "libsodium"
+      ];
+
+      ldflags = [
+        "-s"
+        "-w"
+        "-X github.com/wal-g/wal-g/cmd/pg.walgVersion=${version}"
+        "-X github.com/wal-g/wal-g/cmd/pg.gitRevision=${src.rev}"
+      ];
+
+      postInstall = ''
+        mv $out/bin/pg $out/bin/wal-g
+        installShellCompletion --cmd wal-g \
+          --bash <($out/bin/wal-g completion bash) \
+          --zsh <($out/bin/wal-g completion zsh)
+      '';
+
+      meta = with lib; {
+        homepage = "https://github.com/wal-g/wal-g";
+        license = licenses.asl20;
+        description = "Archival restoration tool for PostgreSQL";
+        mainProgram = "wal-g";
+        maintainers = [ samrose ];
+      };
+    };
+in
+{
+  # wal-g v2.0.1
+  wal-g = walGCommon {
+    version = "2.0.1";
+    sha256 = "sha256-5mwA55aAHwEFabGZ6c3pi8NLcYofvoe4bb/cFj7NWok="; # Replace with actual sha256 for v2.0.1
+    vendorHash = "sha256-BbQuY6r30AkxlCZjY8JizaOrqEBdv7rIQet9KQwYB/g="; # Replace with actual vendorHash for v2.0.1
   };
 
-  vendorHash = "sha256-YDLAmRfDl9TgbabXj/1rxVQ052NZDg3IagXVTe5i9dw=";
-
-  nativeBuildInputs = [ installShellFiles ];
-
-  buildInputs = [
-    brotli
-    libsodium
-  ];
-
-  subPackages = [ "main/pg" ];
-
-  tags = [
-    "brotli"
-    "libsodium"
-  ];
-
-  ldflags = [
-    "-s"
-    "-w"
-    "-X github.com/wal-g/wal-g/cmd/pg.walgVersion=${version}"
-    "-X github.com/wal-g/wal-g/cmd/pg.gitRevision=${src.rev}"
-  ];
-
-  postInstall = ''
-    mv $out/bin/pg $out/bin/wal-g
-    installShellCompletion --cmd wal-g \
-      --bash <($out/bin/wal-g completion bash) \
-      --zsh <($out/bin/wal-g completion zsh)
-  '';
-
-  meta = with lib; {
-    homepage = "https://github.com/wal-g/wal-g";
-    license = licenses.asl20;
-    description = "Archival restoration tool for PostgreSQL";
-    mainProgram = "wal-g";
-    maintainers = [ samrose ];
+  # wal-g v3.0.5
+  wal-g-3 = walGCommon {
+    version = "3.0.5";
+    sha256 = "sha256-wVr0L2ZXMuEo6tc2ajNzPinVQ8ZVzNOSoaHZ4oFsA+U=";
+    vendorHash = "sha256-YDLAmRfDl9TgbabXj/1rxVQ052NZDg3IagXVTe5i9dw=";
   };
 }
