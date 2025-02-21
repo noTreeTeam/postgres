e4e56a3cab50b9e36d4c81d317a3bf6425ca7589
Sam Rose <samuel@supabase.io>
2025-02-21 15:57:22 -0500

    chore: try to resolve pgjwt for pg 15

--- ebssurrogate/files/unit-tests/unit-test-01.sql
+++ ebssurrogate/files/unit-tests/unit-test-01.sql
@@ -5,37 +5,37 @@ DO $$
 DECLARE
     extension_array text[];
     orioledb_available boolean;
+    pg_version integer;
 BEGIN
+    -- Get PostgreSQL version (as integer, e.g., 15 for PostgreSQL 15.x)
+    SELECT current_setting('server_version_num')::integer / 10000 INTO pg_version;
+    
     -- Check if orioledb is available
     SELECT EXISTS (
         SELECT 1 FROM pg_available_extensions WHERE name = 'orioledb'
     ) INTO orioledb_available;
 
-    -- If available, create it and add to the expected extensions list
+    -- Base extensions list
+    extension_array := ARRAY[
+        'plpgsql',
+        'pg_stat_statements',
+        'pgsodium',
+        'pgtap',
+        'pg_graphql',
+        'pgcrypto',
+        'uuid-ossp',
+        'supabase_vault'
+    ];
+    
+    -- Add pgjwt if PostgreSQL version is 15 or higher
+    IF pg_version >= 15 THEN
+        extension_array := array_append(extension_array, 'pgjwt');
+    END IF;
+    
+    -- Add orioledb if available
     IF orioledb_available THEN
         CREATE EXTENSION IF NOT EXISTS orioledb;
-        extension_array := ARRAY[
-            'plpgsql',
-            'pg_stat_statements',
-            'pgsodium',
-            'pgtap',
-            'pg_graphql',
-            'pgcrypto',
-            'uuid-ossp',
-            'supabase_vault',
-            'orioledb'
-        ];
-    ELSE
-        extension_array := ARRAY[
-            'plpgsql',
-            'pg_stat_statements',
-            'pgsodium',
-            'pgtap',
-            'pg_graphql',
-            'pgcrypto',
-            'uuid-ossp',
-            'supabase_vault'
-        ];
+        extension_array := array_append(extension_array, 'orioledb');
     END IF;
 
     -- Set the array as a temporary variable to use in the test
@@ -47,7 +47,6 @@ SELECT plan(8);
 SELECT extensions_are(
     string_to_array(current_setting('myapp.extensions'), ',')::text[]
 );
-
 
 SELECT has_schema('pg_toast');
 SELECT has_schema('pg_catalog');
