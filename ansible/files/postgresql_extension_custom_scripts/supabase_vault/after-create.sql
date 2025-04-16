631096c2ffad887b7e99ce02f1be8a92a488c806
Bobbie Soedirgo <bobbie@soedirgo.dev>
2025-04-16 21:26:34 +0800

    fix: missing Vault privileges for postgres

--- ansible/files/postgresql_extension_custom_scripts/supabase_vault/after-create.sql
+++ ansible/files/postgresql_extension_custom_scripts/supabase_vault/after-create.sql
@@ -1,5 +1,5 @@
 grant usage on schema vault to postgres with grant option;
-grant select, delete on vault.secrets, vault.decrypted_secrets to postgres with grant option;
+grant select, delete, truncate, references on vault.secrets, vault.decrypted_secrets to postgres with grant option;
 grant execute on function vault.create_secret, vault.update_secret, vault._crypto_aead_det_decrypt to postgres with grant option;
 
 -- service_role used to be able to manage secrets in Vault <=0.2.8 because it had privileges to pgsodium functions
